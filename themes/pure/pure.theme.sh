4ac75ad15eae94ed5ca292c8edfeb2cc54c58689
joe <kinjoyuji@gmail.com>
2020-10-24 00:00:15 +0900

    fix theme

--- themes/pure/pure.theme.sh
+++ themes/pure/pure.theme.sh
@@ -33,9 +33,9 @@ pure_prompt() {
 
     # make it work
     case $(id -u) in
-        0) PS1="$ps_root@$ps_host$(scm_prompt):$ps_path$ps_root_mark"
+        0) PS1="$(scm_prompt):$ps_path$ps_root_mark"
             ;;
-        *) PS1="$ps_user@$ps_host$(scm_prompt):$ps_path$ps_user_mark"
+        *) PS1="$(scm_prompt):$ps_path$ps_user_mark"
             ;;
     esac
 }
